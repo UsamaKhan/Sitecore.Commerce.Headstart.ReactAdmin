7900f3987e01008e894d345a8ae6f924e6c8edc9
Usama Khan <usama_khan@bradycorp.com>
2023-11-21 13:13:19 -0600

    feat: :package: Update NextJS to version 14
    Updated NextJS to version 14.0.3. Updated next config by commenting out `pwa` and `ignoreBuildErrors` as they are unrecognized.

--- next.config.js
+++ next.config.js
@@ -1,25 +1,26 @@
 /** @type {import('next').NextConfig} */
-
-module.exports = {
+const nextConfig = {
   devIndicators: {
     autoPrerender: true
   },
-  pwa: {
-    disable:
-      process.env.NODE_ENV === "development" ||
-      process.env.NODE_ENV === "preview" ||
-      process.env.NODE_ENV === "production",
-    // delete two lines above to enable PWA in production deployment
-    // add your own icons to public/manifest.json
-    // to re-generate manifest.json, you can visit https://tomitm.github.io/appmanifest/
-    dest: "public",
-    register: true
-  },
+  // pwa: {
+  //   disable:
+  //     process.env.NODE_ENV === "development" ||
+  //     process.env.NODE_ENV === "preview" ||
+  //     process.env.NODE_ENV === "production",
+  //   // delete two lines above to enable PWA in production deployment
+  //   // add your own icons to public/manifest.json
+  //   // to re-generate manifest.json, you can visit https://tomitm.github.io/appmanifest/
+  //   dest: "public",
+  //   register: true
+  // },
   swcMinify: true,
   reactStrictMode: true,
   eslint: {
     dirs: ["src"]
-  },
+  }
   // !! WARN !!
-  ignoreBuildErrors: true
+  // ignoreBuildErrors: true
 }
+
+module.exports = nextConfig
